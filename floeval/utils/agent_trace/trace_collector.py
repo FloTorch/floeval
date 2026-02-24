0582eb4c516f14b156fc381732f0754c5d8b0fef
shivatantarapelly-fl <shiva.tantarapelly@fissionlabs.com>
2026-02-24 14:01:31 +0530

    missed to commit

--- floeval/utils/agent_trace/trace_collector.py
+++ floeval/utils/agent_trace/trace_collector.py
@@ -54,7 +54,9 @@ class TraceCollector:
             return asyncio.run(self.acollect(partial_samples))
         return self._collect_sync(partial_samples)
 
-    async def acollect(self, partial_samples: list[PartialAgentSample]) -> list[AgentSample]:
+    async def acollect(
+        self, partial_samples: list[PartialAgentSample]
+    ) -> list[AgentSample]:
         """Async interface."""
         patch_openai()
 
@@ -67,7 +69,9 @@ class TraceCollector:
         finally:
             unpatch_openai()
 
-    def _collect_sync(self, partial_samples: list[PartialAgentSample]) -> list[AgentSample]:
+    def _collect_sync(
+        self, partial_samples: list[PartialAgentSample]
+    ) -> list[AgentSample]:
         """Sync collection."""
         patch_openai()
 
